297945e9973ba1171b409eda2a4048e1089ef5cc
juniyasyos <juniyasyos@gmail.com>
2025-08-09 20:20:50 +0700

    update settings

--- database/settings/2025_01_08_152510_create_kaido_settings.php
+++ database/settings/2025_01_08_152510_create_kaido_settings.php
@@ -11,7 +11,7 @@ return new class extends SettingsMigration
         $this->migrator->add('KaidoSetting.contact_email', '');
         $this->migrator->add('KaidoSetting.site_active', true);
         $this->migrator->add('KaidoSetting.registration_enabled', false);
-        $this->migrator->add('KaidoSetting.login_enabled', false);
+        $this->migrator->add('KaidoSetting.login_enabled', true);
         $this->migrator->add('KaidoSetting.password_reset_enabled', false);
         $this->migrator->add('KaidoSetting.sso_enabled', false);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/login', function () {
-    return redirect()->route('filament.admin.auth.login');
-})->name('login');
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
