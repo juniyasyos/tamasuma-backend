0d5e12253ee61bb24b0f58a03012adbb76be9184
Ahmad Ilyas <111669845+juniyasyos@users.noreply.github.com>
2025-08-09 09:42:43 +0700

    Add Partner model support and Filament resource

---
+++ app/Filament/Resources/PartnerResource/Pages/CreatePartner.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PartnerResource\Pages;
+
+use App\Filament\Resources\PartnerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePartner extends CreateRecord
+{
+    protected static string $resource = PartnerResource::class;
+    protected static bool $canCreateAnother = false;
+
+    //customize redirect after create
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Filament/Resources/PartnerResource/Pages/EditPartner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\PartnerResource\Pages;
+
+use App\Filament\Resources\PartnerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPartner extends EditRecord
+{
+    protected static string $resource = PartnerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+        ];
+    }
+
+    //customize redirect after create
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
