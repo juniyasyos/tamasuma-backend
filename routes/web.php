5f268ec8300cc287fef59d4664d60cbf932ea344
Ahmad Ilyas <111669845+juniyasyos@users.noreply.github.com>
2025-08-09 19:06:56 +0700

    Define Filament login route and redirect

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Filament\Pages\Login as FilamentLogin;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/filament/login', FilamentLogin::class)
+    ->name('filament.admin.auth.login');
+
+Route::get('/login', function () {
+    return redirect()->route('filament.admin.auth.login');
+})->name('login');
 
 // Route::get('/', function () {
 //     return view('welcome');
